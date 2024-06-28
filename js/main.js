9b2dc118144814716a1fb42f7309ead37e296b28
Francesco <francesco.dente2003@gmail.com>
2024-06-28 12:24:08 +0200

    1.0.2: fix :ex complete, bonus

--- js/main.js
+++ js/main.js
@@ -18,23 +18,26 @@
 const app = Vue.createApp({
    data(){
       return {
-         email: '',
-         emails: []           
+         emails: [],        
       }
    },
    methods: {
 
    },
    mounted() {
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-         .then(function (call){
-            this.email = call.data.response;
-            console.log(email);
+      for (let i = 0; i < 10; i++) {
+         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+         .then(call => {
+            const result = call.data.response;
+            console.log(result);
+
+            this.emails.push(result);
          }).catch(
             function (error) {
                console.log(error);
             }
          );
+      }
    }
 })
 
